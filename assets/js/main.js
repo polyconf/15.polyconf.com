1016af8197b3484a963ff4b522aed6ed0112fe86
Paweł Wodkowski <pawel@MacBook-Pro-Pawe.local>
2015-02-18 14:16:49 +0100

    New version

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	
+
+	// MENU OVERLAY
+
+	$("nav#overlay").hide();
+	$("#menu-link").click(function(){
+		$("nav#overlay").fadeIn();			
+	})  
+	$("nav#overlay a, #close-menu-link").click(function(){
+		$("nav#overlay").fadeOut();
+	})  
+
+	if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+
+		$(".fixed").each(function(i,e){
+			
+			$(this).css("min-height",$(window).height());
+			$(this)
+			.find("section.content")
+			.css("min-height",$(window).height())
+			.attr({
+				"data-anchor-target":"#"+$(this).attr("id"),
+				"data-bottom-top":"opacity: 1",
+				"data-top-bottom":"opacity: 1"
+			})
+			
+			$(this)
+			.find(".text")
+			.attr({
+				"data-anchor-target":"#"+$(this).attr("id"),
+				"data-bottom-top":"top: 70%",
+				"data-top-bottom":"top: 30%"
+			})
+	
+		
+		});
+	
+		$(".static").each(function(i,e){
+//			$(this).css("min-height",$(window).height());
+		})
+
+	  var s = skrollr.init({
+			forceHeight: false
+		});
+
+	}
+	
+});
